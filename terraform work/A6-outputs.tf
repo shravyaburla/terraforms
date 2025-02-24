990bd9bd54e148384d6f5c0474c006f978e3f741
shravyaburla <shravya083@gmail.com>
2025-02-24 10:57:48 +0530

    Create A6-outputs.tf

---
+++ terraform work/A6-outputs.tf
@@ -0,0 +1,47 @@
+resource "aws_lb" "main" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_alb.id]
+  subnets            = [aws_subnet.subnet_az1.id, aws_subnet.subnet_az2.id]
+
+  enable_deletion_protection = false
+  enable_http2               = true
+  idle_timeout               = 60
+
+  tags = {
+    Name = "my-alb"
+  }
+}
+
+resource "aws_lb_target_group" "app_target_group" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    interval            = 30
+    path                = "/"
+    port                = "80"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "app-target-group"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_target_group.arn
+  }
+}
